60e97e278703d7c70c6ffe844a5bdcce348a53fb
Nolhan <nolhan.marteau@gmail.com>
2026-03-12 21:07:59 +0100

    Feat : Update du composant Card

--- Frontend-Portfolio/src/components/ui/card.jsx
+++ Frontend-Portfolio/src/components/ui/card.jsx
@@ -1,7 +1,8 @@
-export default function Card() {
+export default function Card({children, Description}) {
     return (
         <div class="card">
-                <h2 class="card-title">100</h2>
+                <h2 className="card-title">{children}</h2>
+                <p className="card-description">{Description}</p>
         </div>
     );
 }
