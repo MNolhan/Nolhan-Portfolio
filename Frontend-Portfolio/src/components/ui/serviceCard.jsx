73500c82f236e71362d28e43d55aaccf318caa11
Nolhan <nolhan.marteau@gmail.com>
2026-03-13 20:08:14 +0100

    Feat : Ajout Page Service + Ajout Icons dans les Cards

--- Frontend-Portfolio/src/components/ui/serviceCard.jsx
+++ Frontend-Portfolio/src/components/ui/serviceCard.jsx
@@ -1,8 +1,8 @@
 export default function serviceCard({Icone, children, description}){
     return(
         <div className="serviceCard">
-            <div className="icone">
-                <p>{Icone}</p>
+            <div className="serviceCard__icone">
+                <Icone />
             </div>
             <h3 className="serviceCard__title">{children}</h3>
             <p className="serviceCard__description">{description}</p>
