2b6d2e08b60b22ebb67b14ee4325879107fef57e
Nolhan <nolhan.marteau@gmail.com>
2026-03-21 10:43:51 +0100

    Feat : Ajout de styles responsives pour les sections About, Project et Service

--- Frontend-Portfolio/src/pages/project.jsx
+++ Frontend-Portfolio/src/pages/project.jsx
@@ -12,6 +12,7 @@ export default function project(){
                     <p className="project__subtitle">Une sélection de mes réalisations</p>
                     <div className="project__card-list">
                         <Card type="Web Development" description="Portfolio présentant mes projets, mes compétences et mon expérience en développement web." titre="Portfolio" url="https://github.com/MNolhan/Nolhan-Portfolio"></Card>
+                        <Card type="Web Development" description="Portfolio présentant mes projets, mes compétences et mon expérience en développement web." titre="Portfolio" url="https://github.com/MNolhan/Nolhan-Portfolio"></Card>
                     </div>
                 </div>
             </div>
