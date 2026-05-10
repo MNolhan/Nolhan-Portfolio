8499de4256af41216aead52e26dce18a9104926f
Nolhan <nolhan.marteau@gmail.com>
2026-05-10 11:19:00 +0200

    Feat : Réorganisation des routes utilisateur dans le fichier server.js

--- Backend-Portfolio/src/server.js
+++ Backend-Portfolio/src/server.js
@@ -1,12 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+//Import Routes
+import CreateUser from './routes/users/CreateUser.js';
+import ReadUser from './routes/users/ReadUser.js';
+import UpdateUser from './routes/users/UpdateUser.js';
+import DeleteUser from './routes/users/DeleteUser.js';
+import LoginUser from './routes/users/LoginUser.js';
+
 dotenv.config();
 const app = express();
+app.use(express.json());
 
-app.get('/test', (req, res) => {
-    res.send('Hello World!');
-});
+//Routes
+app.use('/CreateUser', CreateUser);
+app.use('/Users', ReadUser);
+app.use('/UpdateUser', UpdateUser);
+app.use('/DeleteUser', DeleteUser);
+app.use('/Login', LoginUser);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
