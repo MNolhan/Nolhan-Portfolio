2b72aad00b4a1d0ca72a315056c83bb66a76ac3e
Nolhan <nolhan.marteau@gmail.com>
2026-06-06 15:29:26 +0200

    Feat: Mise à jour de la structure du composant UpdateProject avec ajout de la gestion des boutons

--- Frontend-Portfolio/src/components/layouts/project/updateproject.jsx
+++ Frontend-Portfolio/src/components/layouts/project/updateproject.jsx
@@ -6,16 +6,17 @@ export default function UpdateProject() {
         <dialog className="panel panel--popup" id="UpdateProject">
             <h1 className="panel__title">Modifier un Project</h1>
             <div className="panel__content">
+                <p className="panel__message">Non Implémenté</p>
                 <div className="panel__buttons">
-                    
-
+                    <Bouton variant="secondary" type="submit">Mettre à Jour</Bouton>
+                    <Bouton variant="primary" type="button" 
+                        onClick={() => 
+                            document.getElementById("UpdateProject").close()
+                        }>
+                        Fermer
+                    </Bouton>
                 </div>
-                <Bouton variant="primary" type="button" 
-                    onClick={() => 
-                        document.getElementById("UpdateProject").close()
-                    }>
-                    Fermer
-                </Bouton>
+                
             </div>
         </dialog>
     );
