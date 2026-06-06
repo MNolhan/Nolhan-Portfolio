8571ac2df02cfb64c61f2913ac2f1af0c23773dc
Nolhan <nolhan.marteau@gmail.com>
2026-06-06 13:32:18 +0200

    Feat: Ajout de la fonctionnalité de suppression de projet avec gestion des messages de succès et d'erreur

--- Frontend-Portfolio/src/components/layouts/project/deleteproject.jsx
+++ Frontend-Portfolio/src/components/layouts/project/deleteproject.jsx
@@ -2,21 +2,59 @@ import { useState } from "react";
 import Bouton from "../../ui/button";
 
 export default function DeleteProject() {
+    const [name, setName] = useState("");
+    const [message, setMessage] = useState("");
+
+const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch("http://localhost:3000/DeleteProject", {
+                method: "DELETE",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ name }),
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {  
+                setMessage("Suppression du Projet Réussi");
+                setName("");
+                setTimeout(() => { window.location.reload(); }, 1000);
+                return;
+            }
+
+            setMessage(data.message);
+
+        } catch (error) {
+            console.error(error);
+            setMessage("Impossible de contacter le serveur.");
+        }
+    };
+
     return (
         <dialog className="panel panel--popup" id="DeleteProject">
             <h1 className="panel__title">Supprimer un Project</h1>
-            <div className="panel__content">
-                <div className="panel__buttons">
-                    
+            <form className="panel__content" onSubmit={handleSubmit}>
 
+                <input className="input" placeholder="Nom du Projet" value={name} onChange={(e) => setName(e.target.value)}></input>
+
+                {message && <p className="panel__message">{message}</p>}
+
+                <div className="panel__buttons">
+                    <Bouton variant="secondary" type="submit">Supprimer</Bouton>
+                    <Bouton variant="primary" type="button" 
+                        onClick={() => {
+                            document.getElementById("DeleteProject").close();
+                            setName("");
+                            setMessage("");
+                            }
+                        }>
+                        Fermer
+                    </Bouton>
                 </div>
-                <Bouton variant="primary" type="button" 
-                    onClick={() => 
-                        document.getElementById("DeleteProject").close()
-                    }>
-                    Fermer
-                </Bouton>
-            </div>
+
+            </form>
         </dialog>
     );
 }
