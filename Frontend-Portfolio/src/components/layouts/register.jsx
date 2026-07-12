632750b07a0962f74841fb94b85ad5a79ab17602
Nolhan <nolhan.marteau@gmail.com>
2026-07-12 22:20:18 +0200

    Style: Run de prettier pour une meilleure lisibilitée des fichiers

--- Frontend-Portfolio/src/components/layouts/register.jsx
+++ Frontend-Portfolio/src/components/layouts/register.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import Bouton from '../ui/button'
 import Login from './login'
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL
 
 export default function Register() {
   const [name, setName] = useState('')
--- Frontend-Portfolio/src/pages/project.jsx
+++ Frontend-Portfolio/src/pages/project.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import Card from '../components/ui/projectCard'
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL
 
 export default function Project() {
   const [projects, setProjects] = useState([])
