02f38c3a62dcd37c38db6b08e9c0b9d7db5dc329
Nolhan <nolhan.marteau@gmail.com>
2026-08-04 13:29:06 +0200

    Feat: Mise à jour du routage pour utiliser AppRouterV2 et ajout du nouveaux styles pour le conteneur.

--- Frontend-Portfolio/src/App.jsx
+++ Frontend-Portfolio/src/App.jsx
@@ -2,7 +2,7 @@ import AppRouterV1 from './v1/router/AppRouter'
 import AppRouterV2 from './v2/router/AppRouter'
 
 function App() {
-  return <AppRouterV1 />
+  return <AppRouterV2 />
 }
 
 export default App
--- Frontend-Portfolio/src/v2/router/AppRouter.jsx
+++ Frontend-Portfolio/src/v2/router/AppRouter.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 export default function AppRouter() {
   return (
     <BrowserRouter>
+    <div className="containerv2">
+      <h1>test</h1>
+    </div>
     </BrowserRouter>
   )
 }
