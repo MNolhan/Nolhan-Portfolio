cd167428941cee4244f481e0d97938d9c1794d53
Nolhan <nolhan.marteau@gmail.com>
2026-08-11 18:35:55 +0200

    Fix : Ajout d'un timeout de 500ms sur la lecture du nombre de reload après chaque reload afin de laisser le temps à l'incrémentation

--- Backend-Portfolio/src/routes/analytics/ReadReload.js
+++ Backend-Portfolio/src/routes/analytics/ReadReload.js
@@ -8,19 +8,23 @@ router.get("/", async (req , res) => {
 
     try {
 
+        setTimeout(async () => {
+            console.log('Attente de 500ms avant de lire le nombre de reloads...');
+        }, 500);
+
         const [rows] = await pool.execute(
             `Select nombre from visits where id = 1`
         );
 
         res.status(200);
-        console.log('Nombre de visites lu avec succès :', rows);
+        console.log('Nombre de reloads lu avec succès :', rows);
         res.json(rows);
 
     } catch (error) {
 
         res.status(500);
-        console.error('Erreur lors de la lecture du nombre de visites :', error);
-        res.json({ message : "Erreur Serveur" });
+        console.error('Erreur lors de la lecture du nombre de reloads :', error);
+        res.json({ message : "Erreur Serveur lors de la lecture du nombre de reloads" });
 
     }
 
