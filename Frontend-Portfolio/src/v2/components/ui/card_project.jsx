579bd429eed3d97882375a483b7ea3be9a424425
Nolhan <nolhan.marteau@gmail.com>
2026-08-11 17:56:35 +0200

    Feat : Ajout sur les cards Projects le nombre de Stars obtenu sur le projet github

--- Frontend-Portfolio/src/v2/components/ui/card_project.jsx
+++ Frontend-Portfolio/src/v2/components/ui/card_project.jsx
@@ -1,19 +1,48 @@
 import Bouton from "./bouton";
 import CardProjectComponent from "./card_project_component";
+import StarsIcon from "../Icon/stars-icons";
+import { useEffect, useState } from "react";
 
-export default function CardProject({ 
-    type,
-    title,
-    description,
-    live_url,
-    github_url,
-}){
+export default function CardProject({ type, title, description, live_url, github_url,}){
+
+    const NomUtilisateur_Github = github_url.split("/")[3];
+    const NomProjet_Github = github_url.split("/")[4];
+
+    const repository = `https://api.github.com/repos/${NomUtilisateur_Github}/${NomProjet_Github}`;
+    const [repositoryData, setRepositoryData] = useState(null);
+
+    const fetchRepositoryData = async () => {
+        try {
+            const response = await fetch(repository);
+
+            if (!response.ok) {
+                throw new Error('Impossible de récupérer les données du dépôt GitHub.');
+            } else {
+                const data = await response.json();
+                setRepositoryData(data);
+            }
+
+        } catch (error) {
+            console.error('Erreur lors de la récupération des données du dépôt GitHub: ', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchRepositoryData();
+    }, [repository]);
+
     return(
         <div className="CardProject">
 
             <CardProjectComponent type={type} />
 
-            <h3 className="CardProject__title">{title}</h3>
+            <div className="CardProject__header">
+                <h3 className="CardProject__title">{title}</h3>
+                <p className="CardProject__stars">
+                    <StarsIcon /> {repositoryData ? repositoryData.stargazers_count : 0}
+                </p>
+            </div>
+
             <p className="CardProject__description">{description}</p>
 
             <div className="CardProject__buttons">
