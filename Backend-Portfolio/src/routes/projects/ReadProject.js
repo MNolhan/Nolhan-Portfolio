096c2caf2d472a700a7f8fd1d770f67f2d6b4f73
Nolhan <nolhan.marteau@gmail.com>
2026-08-12 10:47:00 +0200

    Fix : Simplification de l'extraction des identifiants dans le parcours détaillé du projet

--- Backend-Portfolio/src/routes/projects/ReadProject.js
+++ Backend-Portfolio/src/routes/projects/ReadProject.js
@@ -34,12 +34,9 @@ const idSchema = z.object({
 
 router.get("/:id", async (req , res) => {
 
-    const { id } = req.params;
-    
-
     try {
 
-        const parsedId = idSchema.parse({ id: Number(id) });
+        const parsedId = idSchema.parse({ id: Number(req.params.id) });
 
         const [rows] = await pool.execute(
             `Select * from projects where id = ?`, [parsedId.id]
